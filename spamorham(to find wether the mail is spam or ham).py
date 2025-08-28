b83d6a92cdca20041f17c7587fcb07998a27c890
keshav maheswari <km9241@srmist.edu.in>
2025-08-28 22:52:12 +0530

    Update spamorham(to find wether the mail is spam or ham).py

--- spamorham(to find wether the mail is spam or ham).py
+++ spamorham(to find wether the mail is spam or ham).py
@@ -1,7 +1,7 @@
 # by using sklearn.naive_bayes to train a spam classifier most of the code is just loading our training data into a pandas DataFrame that we can play with
 '''The goal is to:
 Read emails from a folder structure (spam and ham)
-Extract only the message body,store it in a labeled dataset (as spam or ham),later use it for spam detection using machine learning
+Extract only the message body,store it in a labeled dataset (as spam or ham),later use it for spam detection using machine learning(from sklearn.naive_bayes import MultinomialNB)
 '''
 import io #Used for opening files with a specific encoding (like latin1)
 import os #Lets you interact with the file system ( walking through folders).
@@ -16,15 +16,13 @@ So we need to Convert text into numbers Thats where CountVectorizer comes in —
 from sklearn.naive_bayes import MultinomialNB
 ''' How multinomialNB Works in Practice
 Training: You pass a document-term matrix (from CountVectorizer) and the correct class labels ('spam' or 'ham').
-Model learns:
-how often each word appears in spam and ham
-The overall probability of spam and ham emails
-prediction:-
-For a new email it multiplies the probabilities of the words it contains and chooses the class with the highest probability'''
-'''Naive Bayes means a method of classifying things using probabilities based on past data, but with the naive assumption that all features are independent from each other.
-In simple words:It looks at your data and calculates how likely each class is
-It asssumes that every feature (like words in a sentence ) contributes to the decision independently.
-Then it picks the class with the highest probability.'''
+Model learns: how often each word appears in spam and ham folders
+and the overall probability of spam and ham emails
+prediction:-For a new email it multiplies the probabilities of the words it contains and chooses the class with the highest probability(bayerns therorum sort of thing)'''
+'''Naive Bayes means a method of classifying things using probabilities based on past data, but with the naive assumption that all features are independent from each other
+in simple words:It looks at your data and calculates how likely each class is
+it asssumes that every feature (like words in a sentence ) contributes to the decision independently
+then it picks the class with the highest probability.'''
 # using a bag of word model
 def readFiles(path):
     for root, dirnames, filenames in os.walk(path):
@@ -66,9 +64,10 @@ classifier = MultinomialNB()
 targets = data['class'].values
 classifier.fit(counts, targets)
 
-examples = ['Free Viagra now!!!', "Hi Bob, how about a game of golf tomorrow?"]
+examples = ['Free Viagra now!', "Hi drake how about we go to diddy party?"]
 example_counts = vectorizer.transform(examples)
 predictions = classifier.predict(example_counts)
 predictions
+
 
 
